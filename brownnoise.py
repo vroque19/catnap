ff832b69f3f1da9bfc32f429a4629cb622e5bb3b
David Frias <frias901@MacBook-Pro-309.lan>
2025-03-01 20:43:19 -0800

    Created functions for generating brown noise and white noise.

--- brownnoise.py
+++ brownnoise.py
@@ -1,10 +1,33 @@
 """Code for generating brown noise on the Raspberry Pi 5"""
 
 import numpy as np
+import sounddevice as sd
 
 def generate_white_noise(duration, sample_rate):
     """Generates white noise"""
 
-    noise = numpy.random.randn(int(duration * sample_rate))
+    num_samples = duration * sample_rate
+    noise = np.random.randn(int(num_samples))
 
     return noise
+
+def generate_brown_noise(duration, sample_rate):
+    """Generates brown noise"""
+
+    # Collect samples
+    num_samples = duration * sample_rate
+    
+    # Generate white noise
+    white_noise = np.random.randn(int(num_samples))
+    
+    # Integrate white noise to get brown noise
+    brown_noise = np.cumsum(white_noise)
+
+    # Normalize to prevent clipping
+    brown_noise = brown_noise / np.max(np.abs(brown_noise))
+
+    print(f"Playing {duration} seconds of brown noise.")
+    sd.play(brown_noise, samplerate=sample_rate)
+    sd.wait()
+
+generate_brown_noise(5, 44100)
